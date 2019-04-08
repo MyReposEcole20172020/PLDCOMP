88afc7b5c2504631c833f5bf731207a3e755f2f0
tianjianye <44018955+tianjianye@users.noreply.github.com>
2019-04-08 16:09:20 +0200

    Update testFrontEnd.py

--- testFrontEnd.py
+++ testFrontEnd.py
@@ -19,28 +19,28 @@ def writeIn(filename,contents):
 	f.write(str(contents))
 	f.close()
 	pass
-def execution(filename):
-	obj=subprocess.Popen(["make","testsFrontEnd","file="+str(filename)],
-stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True)
+def execution(filename,root):
+	os.system("make testsFrontEnd folderC="+str(root)+" file="+str(filename))
+	obj=subprocess.Popen(["gcc", "-w", str(root)+"/results/"+str(filename)+"/"+str(filename)+".o","-o",str(root)+"/results/"+str(filename)+"/"+str(filename)+".out"],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True)
 	out_error_list=obj.communicate()[0]
 	return out_error_list
 listFile=[]
 listResult=[]
 listError=[]
 for root,dirs,files in os.walk("./tests/testsFrontEnd",topdown=False):
-	for name in files:
+	for name in sorted(files):
 		if name[-2:] == '.c':
-			newname=os.path.join(root,name)
-			l=len(newname)
-			file=newname[0:l-2]
-			out_error_list=execution(file)
+			l=len(name)
+			file=name[0:l-2]
+			print(file)
+			out_error_list=execution(file,root)
 			listFile.append(file)
 			if(len(out_error_list)!=0):
 				listResult.append("failed")
 			else:
 				listResult.append("success")
 			listError.append(out_error_list)
-			os.system("make testsFrontEndClean file="+str(file))
+			#os.system("make testsFrontEndClean file="+str(file))
 		else:
 			pass
 tab=printDict(listFile,listResult,listError)
