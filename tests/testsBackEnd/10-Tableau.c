4ff3e416c0c30c6ed767f3be7ef946acc4f2ad78
tianjianye <44018955+tianjianye@users.noreply.github.com>
2019-04-10 11:19:38 +0200

    Add files via upload

---
+++ tests/testsBackEnd/10-Tableau.c
@@ -0,0 +1,13 @@
+int square (int a) {
+    return a*a;
+}
+
+char main(int arg){
+	char tab[10];
+	int i ;
+	for(i = 0; i < 10; i++) {
+	    tab[i] = 'b';
+	    putchar('\n');
+	}
+	return tab[5] = '9';
+}
