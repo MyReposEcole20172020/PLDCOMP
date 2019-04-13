27c6b4a8e4f9222a07a514dcff00750fcd3f669f
tianjianye <44018955+tianjianye@users.noreply.github.com>
2019-04-13 19:33:07 +0200

    Delete out.sh

--- out.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-	rm -rf ./*.o ./*.s out.pdf a.out
-	filename=$(echo $1|cut -d . -f1)
-	cat $filename.c
-	make build file=$filename ||exit "$$?"
-	echo "output:"	
-	./a.out
-	echo "\nreturn:\n"$?
