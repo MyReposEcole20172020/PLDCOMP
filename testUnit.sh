d0e9642b5a87a981cbd68bcaab8b4d94f19ba3da
tianjianye <44018955+tianjianye@users.noreply.github.com>
2019-04-14 15:57:52 +0200

    20190414_15:57

--- testUnit.sh
+++ testUnit.sh
@@ -5,12 +5,12 @@
 	length=$(($length-2))
 	filename=$(echo $1|cut -c1-$length)
 	cat "$filename.c"
-	make back file=$filename path=$filepath
+	make --no-print-directory backUnit file=$filename path=$filepath||exit
 	output2=$($filepath"/a.out")
 	return2=$(echo $?)
 	echo "\noutput comp:"$output2
 	echo "return value comp:"$return2"\n"
-	gcc -w "$filename.c" -o "$filepath/a.out"
+	gcc "$filename.c" -o "$filepath/a.out"
 	output1=$($filepath/a.out)
 	return1=$(echo $?)
 	echo "\noutput gcc:"$output1
